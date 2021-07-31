0a9085bdbc908d80314355c6c2ef2de25cfbd559
jjcourtney <jcourtneywork@gmail.com>
2021-07-31 22:40:14 +0100

    added questions for user

--- src/index.js
+++ src/index.js
@@ -5,12 +5,65 @@ const util = require('util');
 
 const updateReadme = util.promisify(fs.writeFile);
 
+/*
+Description
+## Table of Contents (Optional)
+## Installation
+## Usage
+## Credits
+## License
+## Badges
+## Features
+## How to Contribute
+## Tests
+*/
+
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'projectName',
       message: 'What is the name of the project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What is the installation?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage?',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'What is the name contributors?',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'What is the license?',
+    },
+    {
+      type: 'input',
+      name: 'badges',
+      message: 'What badges?',
+    },
+    {
+      type: 'input',
+      name: 'features',
+      message: 'What are the features?',
+    },
+    {
+      type: 'input',
+      name: 'contributionInstructions',
+      message: 'What is the instructions to contribute?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What is the tests?',
     },
 
   ]);
