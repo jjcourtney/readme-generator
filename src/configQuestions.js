3571c9531ef2f82edece6957f74d2a1a6034d0ea
jjcourtney <jcourtneywork@gmail.com>
2021-08-01 22:28:50 +0100

    added config questions.js

---
+++ src/configQuestions.js
@@ -0,0 +1,21 @@
+
+const getConfigQuestions = () => [
+    
+  { 
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of the project?',
+  },
+  { 
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of the project?',
+  },
+
+
+]
+
+const promptConfigQuestions = () => {
+    
+}
+
